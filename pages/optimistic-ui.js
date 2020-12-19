469ff8864de70b97f099028777586b1ffa6ade3a
Kien Do <kiendo95@gmail.com>
2020-12-19 16:17:00 +0700

    Put the latest message on the top instead of bottom

--- pages/optimistic-ui.js
+++ pages/optimistic-ui.js
@@ -17,29 +17,33 @@ export default function OptimisticUI() {
   const { data } = useSWR(query, getData)
 
   async function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
+
     // mutate current data to optimistically update the UI
-    mutate(query, {users: [...data.users, {id: uuidv4(), name: text}]}, false)
-    // send text to the API
+    mutate(query, { users: [{ id: uuidv4(), name: text }, ...data.users]}, false)
+
+    // remotely update the data
     const mutation = {
       'query': 'mutation users($name: String!) { insert_users(objects: [{name: $name}]) { affected_rows } }',
       'variables': { name: text}
     };
     await fetch(mutation);
+
     // revalidate
     trigger(mutation);
     setText('')
   }
 
   return <div>
-    <h1>Insert a new user</h1>
+    <h1>Opmitimstic UI</h1>
+    <p style={{ color: '#777' }}>Notice that there's no loading, as updates are too fast it's invisible to human's eye</p>
     <form onSubmit={handleSubmit}>
       <input
         type="text"
         onChange={event => setText(event.target.value)}
         value={text}
       />
-      <button>Create</button>
+      <button>Create Message</button>
     </form>
     <ul>
       {data ? data.users.map(user => <li key={user.id}>{user.name}</li>) : 'loading...'}
